9ce981260e35365ca0271984005a9ae31b19cc6b
myWebname <1501530571@qq.com>
2016-06-30 15:09:43 +0800

    my second write

--- app/app.js
+++ app/app.js
@@ -22,6 +22,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use(express.static(path.join(__dirname, 'views')));
+
+
 app.use('/', routes);
 app.use('/users', users);
 
